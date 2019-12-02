b9b36e940ac74e2f5d483eeb9ece36a7b0e038bd
Samantha <sami.shackelford@gmail.com>
2019-12-02 14:50:00 -0800

    add shop page, collection preview component and collection item

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,7 +49,7 @@ class Directory extends Component {
     return (
       <div className='directory-menu'>
         {
-          this.state.sections.map(({ id, ...otherSectionProps}) => (
+          this.state.sections.map(({ id, ...otherSectionProps }) => (
             <MenuItem key={id} {...otherSectionProps} />
           ))
         }
